a4ffaad2c1834f8bd0662685608898d617aa06ff
Deepanshu Jain <54257243+deepanshux@users.noreply.github.com>
2020-06-08 23:42:47 +0530

    added login page

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.1.0",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-whitelist": "1.3.4"
+}
+// BOTTOM OF METADATA
+});
